ffd7016e8552678e5c357d819be17800a8fcb155
bazamel <basunako@gmail.com>
2026-02-02 09:52:48 +0700

    remove cache files

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/home/bazamel/clients/cgwire/developer-documentation/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/home/bazamel/clients/cgwire/developer-documentation/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
